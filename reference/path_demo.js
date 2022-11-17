039e99a72b426f4e9ab37445da21951be98e7587
Cleophas <cleophasfoy@gmail.com>
2022-11-17 14:30:32 -0600

    practiced using node.js

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -10,4 +10,8 @@ console.log(path.dirname(__filename));
 console.log(path.extname(__filename));
 
 //Create path object
-console.log(path.parse(__filename));
+console.log(path.parse(__filename).base);
+
+//Concatenate paths
+
+console.log(path.join(__dirname, 'test', 'hello.html'));
